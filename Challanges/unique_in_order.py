583feb4d38b6832650f3f79291bafa292e1b82c2
KikatoIV <39209669+KikatoIV@users.noreply.github.com>
2020-07-06 18:25:13 +0200

    Replace With Alphabet Position

--- Challanges/unique_in_order.py
+++ Challanges/unique_in_order.py
@@ -8,8 +8,9 @@ def unique_in_order(iterable):
             lst.append(c)
         stng = c
     return lst
-
 print(unique_in_order('AAAABBBCCDAABBB'))
+
+
 #this was a very clever solution i found
 unique_in_order = lambda l: [z for i, z in enumerate(l) if i == 0 or l[i - 1] != z]
 
